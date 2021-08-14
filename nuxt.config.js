a9bac12e1e7c100a070a7b9c6ff9673cc436da39
SHARON <sharushj690@gmail.com>
2021-08-14 22:14:24 +0530

    removed target: static to test netlify deploy & github auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   telemetry: false,
-  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     htmlAttrs: {
