ed031c8ae13c4b3238d65c4f182d5760af1c276d
SHARON <sharushj690@gmail.com>
2021-08-14 21:17:15 +0530

    added target: 'static' for netlify deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   telemetry: false,
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     htmlAttrs: {
